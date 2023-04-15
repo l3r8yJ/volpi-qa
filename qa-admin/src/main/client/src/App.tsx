cf491c2b40556887995f2fb00991ed28dd8dce0b
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-15 21:38:28 +0300

    base layout

--- qa-admin/src/main/client/src/App.tsx
+++ qa-admin/src/main/client/src/App.tsx
@@ -1,7 +1,13 @@
+import {Header} from "./components/Header";
 
 function App() {
     return (
-        <div></div>
+        <div className={"text-white min-h-screen bg-neutral-800"}>
+            <Header/>
+            <main>
+
+            </main>
+        </div>
     )
 }
 
