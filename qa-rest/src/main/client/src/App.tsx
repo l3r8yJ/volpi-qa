050fb8569b5fc2b7ca42031664c8e9de266c1e47
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-15 23:41:07 +0300

    chat via modal

--- qa-rest/src/main/client/src/App.tsx
+++ qa-rest/src/main/client/src/App.tsx
@@ -1,16 +1,39 @@
 import {Chat} from "./components/Chat";
+import {Modal} from "./components/UI/Modal";
+import {useState} from "react";
+import {ChatBubbleBottomCenterTextIcon} from "@heroicons/react/24/outline";
 
 function App() {
     const isDevMode = process.env.NODE_ENV === 'development'
+    const [chatVisible, setChatVisible] = useState(false)
 
     return (
         <>
             {isDevMode
                 ?
-                <div className={"flex items-center justify-center h-screen"}>
-                    <Chat/>
+                <div className={"flex flex-col items-center justify-center min-h-screen"}>
+                    <button onClick={() => setChatVisible(!chatVisible)}>
+                        <ChatBubbleBottomCenterTextIcon className={"w-10 h-10"}/>
+                    </button>
+                    <Modal
+                        isOpen={chatVisible}
+                        setIsOpen={setChatVisible}
+                    >
+                        <Chat/>
+                    </Modal>
                 </div>
-                : <Chat/>
+                :
+                <>
+                    <button onClick={() => setChatVisible(!chatVisible)}>
+                        <ChatBubbleBottomCenterTextIcon className={"w-10 h-10"}/>
+                    </button>
+                    <Modal
+                        isOpen={chatVisible}
+                        setIsOpen={setChatVisible}
+                    >
+                        <Chat/>
+                    </Modal>
+                </>
             }
         </>
     )
