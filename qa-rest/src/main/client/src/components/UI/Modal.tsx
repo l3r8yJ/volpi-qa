c743eb0c9b2b8c39c3aecb2f9cd91cd6fdfed382
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-16 12:29:01 +0300

    add more adaptive
    now height cant be more that h-screen -20px and on smallscreen it will be placed in center

--- qa-rest/src/main/client/src/components/UI/Modal.tsx
+++ qa-rest/src/main/client/src/components/UI/Modal.tsx
@@ -17,8 +17,8 @@ export const Modal: FC<ModalProps> = ({children, isOpen, setIsOpen}) => {
             className="relative z-[1001]"
         >
             <div className="fixed inset-0 bg-black/30" aria-hidden="true"/>
-            <div className="fixed bottom-0 right-0 flex items-center justify-center p-4">
-                <Dialog.Panel className="rounded-lg relative">
+            <div className="fixed inset-0 md:left-auto md:top-auto flex items-center justify-center p-2 md:pb-4 md:pr-4">
+                <Dialog.Panel className="rounded-lg relative min-w-[250px] w-[370px]">
                     <button
                         className={"absolute text-white top-4 right-4 z-10 hover:text-neutral-300"}
                         onClick={() => setIsOpen(false)}
