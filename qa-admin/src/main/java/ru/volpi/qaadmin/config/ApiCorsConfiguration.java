9fc236d38914e5731c757cca82b79fac5adf2cea
Ivan <clicker.heroes.acg@gmail.com>
2023-04-17 19:55:27 +0300

    imports

--- qa-admin/src/main/java/ru/volpi/qaadmin/config/ApiCorsConfiguration.java
+++ qa-admin/src/main/java/ru/volpi/qaadmin/config/ApiCorsConfiguration.java
@@ -1,10 +1,7 @@
 package ru.volpi.qaadmin.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
