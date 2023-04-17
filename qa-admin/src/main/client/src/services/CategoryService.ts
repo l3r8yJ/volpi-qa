a6a96bdd9179912597c5031c7c9fc2eafe88b11b
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-17 19:47:39 +0300

    base admin app crud (not tested)

--- qa-admin/src/main/client/src/services/CategoryService.ts
+++ qa-admin/src/main/client/src/services/CategoryService.ts
@@ -1,9 +1,26 @@
 import {$api} from "../http/api";
 import {ICategory} from "../types/ICategory";
+import {AxiosResponse} from "axios";
 
 
-export class CategoryService{
-    static fetchAllCategories(){
+export class CategoryService {
+    static fetchAllCategories(): Promise<AxiosResponse<ICategory[]>> {
         return $api.get<ICategory[]>("categories")
+    }
+
+    static fetchCategoryByName(name: string): Promise<AxiosResponse<ICategory>> {
+        return $api.get<ICategory>("category/" + name)
+    }
+
+    static createCategory(name: string): Promise<AxiosResponse<ICategory>> {
+        return $api.post<ICategory>("categories", name)
+    }
+
+    static updateCategory(updatedCategory: ICategory): Promise<AxiosResponse<ICategory>> {
+        return $api.patch<ICategory>("categories" + updatedCategory.id, updatedCategory)
+    }
+
+    static deleteCategory(id: number):Promise<AxiosResponse<ICategory>>{
+        return $api.delete<ICategory>("categories" + id)
     }
 }
--- qa-admin/src/main/client/src/store/reducers/categorySlice.ts
+++ qa-admin/src/main/client/src/store/reducers/categorySlice.ts
@@ -1,31 +1,79 @@
 import {ICategory} from "../../types/ICategory";
 import {createSlice} from "@reduxjs/toolkit";
-import {fetchCategories} from "../actions/categoryAction";
+import {
+    createCategory,
+    deleteCategory,
+    fetchCategories,
+    fetchCategoryByName,
+    updateCategory
+} from "../actions/categoryAction";
 
 
 interface CategoryState {
     categories: ICategory[]
+    currentCategory: ICategory
     loading: "idle" | "pending" | "succeeded" | "failed"
 }
 
 const initialState:CategoryState = {
     categories: [],
+    currentCategory: {} as ICategory,
     loading: "idle",
 }
 
 const categorySlice = createSlice({
     name: "category",
     initialState,
-    reducers:{},
-    extraReducers:(builder) => {
-        builder.addCase(fetchCategories.pending, (state) =>{
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchCategories.pending, (state) => {
+            console.log("pending")
             state.loading = "pending"
-        }).addCase(fetchCategories.fulfilled, (state, action) =>{
+        }).addCase(fetchCategories.fulfilled, (state, action) => {
+            console.log("succeeded")
             state.loading = "succeeded"
             state.categories = action.payload
-        }).addCase(fetchCategories.rejected, (state, action) =>{
+        }).addCase(fetchCategories.rejected, (state, action) => {
+            console.log("failed")
             state.loading = "failed"
             console.log(action.error);
+        })
+
+        builder.addCase(fetchCategoryByName.pending, (state) => {
+            state.loading = "pending"
+        }).addCase(fetchCategoryByName.fulfilled, (state, action) => {
+            state.loading = "succeeded"
+            state.currentCategory = action.payload
+        }).addCase(fetchCategoryByName.rejected, (state, action) => {
+            state.loading = "failed"
+            console.log(action.error)
+        })
+
+        builder.addCase(createCategory.pending, (state) => {
+            state.loading = "pending"
+        }).addCase(createCategory.fulfilled, (state) => {
+            state.loading = "succeeded"
+        }).addCase(createCategory.rejected, (state, action) => {
+            state.loading = "failed"
+            console.log(action.error)
+        })
+
+        builder.addCase(updateCategory.pending, (state) => {
+            state.loading = "pending"
+        }).addCase(updateCategory.fulfilled, (state) => {
+            state.loading = "succeeded"
+        }).addCase(updateCategory.rejected, (state, action) => {
+            state.loading = "failed"
+            console.log(action.error)
+        })
+
+        builder.addCase(deleteCategory.pending, (state) => {
+            state.loading = "pending"
+        }).addCase(deleteCategory.fulfilled, (state) => {
+            state.loading = "succeeded"
+        }).addCase(deleteCategory.rejected, (state, action) => {
+            state.loading = "failed"
+            console.log(action.error)
         })
     }
 })
