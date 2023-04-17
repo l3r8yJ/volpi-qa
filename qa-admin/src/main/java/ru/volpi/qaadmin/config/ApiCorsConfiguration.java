0d3b89447de06c772cd8cd5aba58ab1cfe9fa308
Ivan <clicker.heroes.acg@gmail.com>
2023-04-17 19:54:54 +0300

    cors config

---
+++ qa-admin/src/main/java/ru/volpi/qaadmin/config/ApiCorsConfiguration.java
@@ -0,0 +1,24 @@
+package ru.volpi.qaadmin.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiCorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+}
