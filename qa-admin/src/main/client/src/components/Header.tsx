ace01f30184ddc71d645a173038ca03340e037bc
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-18 17:55:06 +0300

    add more adaptive to header

--- qa-admin/src/main/client/src/components/Header.tsx
+++ qa-admin/src/main/client/src/components/Header.tsx
@@ -3,10 +3,10 @@ import {UserIcon} from "@heroicons/react/24/outline";
 
 export const Header: FC = () => {
     return (
-        <header className={" bg-neutral-900/50 py-4 "}>
-            <div className={"container mx-auto flex items-center justify-between"}>
+        <header className={"bg-neutral-900/50 py-4 border-b border-neutral-500/50"}>
+            <div className={"mx-5 lg:container lg:mx-auto flex items-center justify-between"}>
                 <div className={"font-semibold text-3xl pointer-events-none"}>
-                    Volpi QA admin
+                    Volpi QA <span className={"text-indigo-600"}>admin</span>
                 </div>
                 <div className={"flex space-x-1 cursor-pointer  hover:text-neutral-300 duration-100"}>
                     <span>login</span>
