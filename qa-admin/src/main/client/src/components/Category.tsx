a6c263520312334ff1a4f10503fa70458facb6c9
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-19 10:08:31 +0300

    add deleting category

--- qa-admin/src/main/client/src/components/Category.tsx
+++ qa-admin/src/main/client/src/components/Category.tsx
@@ -1,14 +1,28 @@
 import {FC} from 'react';
 import {ICategory} from "../types/ICategory";
-import {ChevronDownIcon} from "@heroicons/react/24/outline";
+import {XMarkIcon} from "@heroicons/react/24/outline";
+import {useAppDispatch} from "../hooks/redux";
+import {deleteCategory, fetchCategories} from "../store/actions/categoryAction";
 
 export const Category: FC<ICategory> = ({id, questions, name}) => {
+    const dispatch = useAppDispatch()
+    const deleteBtnHandler = async () => {
+        await dispatch(deleteCategory(id))
+        dispatch(fetchCategories())
+    }
     return (
-        <li className={"even:bg-neutral-800 flex items-center cursor-pointer p-2 hover:bg-indigo-800 rounded-lg"}>
+        <li className={"even:bg-neutral-800 flex items-center p-2 hover:bg-indigo-800 rounded-lg"}>
             <div className={"w-10 px-2 text-neutral-400"}>{id}</div>
             <div className={"px-2 flex justify-between w-full"}>
                 <div>{name}</div>
-                <div><ChevronDownIcon className={"w-6 h-6"}/></div>
+                <div className={"flex space-x-2"}>
+                    <div
+                        className={"px-2 hover:text-red-600 cursor-pointer duration-150"}
+                        onClick={deleteBtnHandler}
+                    >
+                        <XMarkIcon className={"w-6 h-6  "}/>
+                    </div>
+                </div>
             </div>
         </li>
     );
