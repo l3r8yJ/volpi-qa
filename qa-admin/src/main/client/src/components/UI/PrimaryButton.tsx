9c3559fc4e68246e3feb9e6b44658aa617b529a3
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-23 12:20:11 +0300

    update props and styles for primary button

--- qa-admin/src/main/client/src/components/UI/PrimaryButton.tsx
+++ qa-admin/src/main/client/src/components/UI/PrimaryButton.tsx
@@ -1,9 +1,9 @@
 import {ButtonHTMLAttributes, FC} from 'react';
 
-export const PrimaryButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({title, ...props}) => {
+export const PrimaryButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({children, className, ...props}) => {
     return (
-        <button className={"bg-indigo-700 px-4 py-2 rounded-lg hover:bg-indigo-600 w-full"} {...props}>
-            {title}
+        <button className={className + " bg-indigo-700 px-4 py-2 rounded-lg hover:bg-indigo-600 w-full max-w-[250px]"} {...props}>
+            {children}
         </button>
     );
 }
