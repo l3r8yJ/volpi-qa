800ec61df5a5d73d36b0723ea27dc0f86b677be9
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-23 17:28:06 +0300

    move questions loading logic to page component

--- qa-admin/src/main/client/src/pages/CategoryName.tsx
+++ qa-admin/src/main/client/src/pages/CategoryName.tsx
@@ -1,13 +1,52 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {MainLayout} from "../components/layouts/MainLayout";
 import {useParams} from "react-router-dom";
+import {QuestionsList} from "../components/questions/QuestionsList";
+import {useAppDispatch, useAppSelector} from "../hooks/redux";
+import {fetchQuestionsByCategory} from "../store/actions/questionAction";
+import {QuestionsForm} from "../components/questions/QuestionsForm";
+import {Sidebar} from "../components/Sidebar";
+import {clearCurrentCategory} from "../store/reducers/categorySlice";
 
 const CategoryName: FC = () => {
     const {name} = useParams()
-
+    const {questions, loading} = useAppSelector(state => state.question)
+    const dispatch = useAppDispatch()
+    if (!name)
+        return (
+            <MainLayout>
+                <div>Ошибка: категория не найдена</div>
+            </MainLayout>
+        )
+    useEffect(() => {
+        dispatch(fetchQuestionsByCategory(name))
+        return() => {
+            dispatch(clearCurrentCategory())
+        }
+    }, [])
     return (
         <MainLayout>
-            {name}
+            <div className={"text-3xl w-full border-b py-4"}>
+                <span className={"opacity-60"}>Категория:</span> {name}
+            </div>
+            {loading === "pending"
+                ? <div>Загрузка...</div>
+                : <>
+                    {questions.length !== 0
+                        ? <div className={"mt-4"}>
+                            <Sidebar><QuestionsForm categoryName={name}/></Sidebar>
+                            <div className={"ml-64 xl:ml-72"}>
+                            <QuestionsList questions={questions}/>
+                            </div>
+                        </div>
+                        : <div className={"mt-4 space-y-4"}>
+                            <div>В этой категории нет ещё ни одного вопроса</div>
+                            <QuestionsForm categoryName={name}/>
+                        </div>
+                    }</>
+            }
+
+
         </MainLayout>
     );
 }
