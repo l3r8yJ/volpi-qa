a68ab7f9a3daa5109a5b27c8bbd552338de3ccd4
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-23 17:21:25 +0300

    update sidebar
    now sidebar is only wrapper with children

--- qa-admin/src/main/client/src/components/Sidebar.tsx
+++ qa-admin/src/main/client/src/components/Sidebar.tsx
@@ -1,29 +1,14 @@
-import {FC, useState} from 'react';
-import {PrimaryButton} from "./UI/PrimaryButton";
-import {Input} from "./UI/Input";
-import {useAppDispatch} from "../hooks/redux";
-import {createCategory, fetchCategories} from "../store/actions/categoryAction";
+import {FC, ReactNode} from 'react';
 
-export const Sidebar: FC = () => {
-    const [inputValue, setInputValue] = useState("")
-    const dispatch = useAppDispatch()
-    const btnHandler = async () => {
-        if(inputValue){
-            await dispatch(createCategory(inputValue))
-            dispatch(fetchCategories())
-            setInputValue("")
-        }
-    }
+interface SidebarProps {
+    children: ReactNode
+}
+
+export const Sidebar: FC<SidebarProps> = ({children}) => {
+
     return (
-        <div className={"fixed flex flex-col w-60 h-96 bg-neutral-800 rounded-lg p-4"}>
-            <div className={"space-y-2 flex flex-col items-center"}>
-                <Input
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                    placeholder={"Название категории"}
-                />
-                <PrimaryButton onClick={btnHandler}>Новая категория</PrimaryButton>
-            </div>
+        <div className={"fixed flex flex-col w-60 rounded-lg p-4 border border-neutral-500/50 shadow-lg shadow-neutral-800/80"}>
+            {children}
         </div>
     );
 }
