91a777ca4c18bcaae2641657060062623c11aeeb
Andrew-Sem <avvyayvayva@gmail.com>
2023-04-28 00:46:29 +0300

    add createStore func
    it is necessary to create different stores with default value in tests

--- qa-admin/src/main/client/src/store/store.ts
+++ qa-admin/src/main/client/src/store/store.ts
@@ -2,13 +2,15 @@ import {configureStore} from "@reduxjs/toolkit";
 import categoryReducer from "./reducers/categorySlice";
 import questionReducer from "./reducers/questionSlice";
 
-export const store = configureStore({
-    reducer:{
-        category: categoryReducer,
-        question: questionReducer
-    }
-})
+export const createStore = () => configureStore({
+        reducer: {
+            category: categoryReducer,
+            question: questionReducer
+        }
+    })
 
+
+export const store = createStore()
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
