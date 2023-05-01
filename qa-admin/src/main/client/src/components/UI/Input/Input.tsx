0fde7894fbf09acecb28b6a4bdb9f603fbe0b581
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-01 20:55:12 +0300

    set label wrapper always for input

--- qa-admin/src/main/client/src/components/UI/Input/Input.tsx
+++ qa-admin/src/main/client/src/components/UI/Input/Input.tsx
@@ -6,7 +6,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input: FC<InputProps> = ({className, label, validateResult = "", ...props}) => {
-    let validateResultInputClass = ""
+    let validateResultInputClass
     let validateResultDescriptionClass = ""
     switch (validateResult) {
         case "выглядит хорошо!":
@@ -22,25 +22,13 @@ export const Input: FC<InputProps> = ({className, label, validateResult = "", ..
 
     }
     return (
-        <>
-            {label
-                ? <label className={"max-w-[250px]"}>
-                    <div className={"text-neutral-400 text-sm ml-1"}>{label}</div>
-                    <input
-                        className={`${validateResultInputClass} ${className} px-4 py-2 rounded-lg outline-none bg-neutral-900 border w-full`}
-                        {...props}
-                    />
-                    {validateResult && <div className={validateResultDescriptionClass}>{validateResult}</div>}
-                </label>
-                : <div>
-                    <input
-                        className={className + " px-4 py-2 rounded-lg outline-none bg-neutral-900 border border-neutral-500/50 max-w-[250px]"}
-                        {...props}
-                    />
-                    {validateResult && <div className={validateResultDescriptionClass}>{validateResult}</div>}
-                </div>
-
-            }
-        </>
+        <label className={"max-w-[250px]"}>
+            {label && <div className={"text-neutral-400 text-sm ml-1"}>{label}</div>}
+            <input
+                className={`${validateResultInputClass} ${className} px-4 py-2 rounded-lg outline-none bg-neutral-900 border w-full`}
+                {...props}
+            />
+            {validateResult && <div className={validateResultDescriptionClass}>{validateResult}</div>}
+        </label>
     );
 }
