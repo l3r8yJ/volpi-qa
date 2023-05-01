869312c145832972f6b0f97af2b7c2767eacabc0
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-02 00:29:17 +0300

    upgrade input tests to new validation logic

--- qa-admin/src/main/client/src/components/UI/Input/Input.test.tsx
+++ qa-admin/src/main/client/src/components/UI/Input/Input.test.tsx
@@ -3,10 +3,14 @@ import {render, RenderResult, screen} from "@testing-library/react";
 import {Input} from "./Input";
 import userEvent from "@testing-library/user-event";
 import {InputTestID} from "../../../constants/testIDs";
+import {createValidateInputValueFunc} from "../../../utils/createValidateInputValue/createValidateInputValueFunc";
 
 
 describe("Input", () => {
+    const showValidation = true
+    const validateFunc = createValidateInputValueFunc()
     const inputValue = 42
+    const setIsValid = vi.fn()
     const onChangeInput = vi.fn()
     const onHoverInput = vi.fn()
     const onBlurInput = vi.fn()
@@ -15,12 +19,15 @@ describe("Input", () => {
     describe("basic props and behaviour", () => {
         beforeEach(() => {
             renderOption = render(
-                <input
+                <Input
                     data-testid={InputTestID}
                     value={inputValue}
                     onMouseEnter={onHoverInput}
                     onMouseLeave={onBlurInput}
                     onChange={(e) => onChangeInput(e.target.value)}
+                    setIsValid={setIsValid}
+                    validateFunc={validateFunc}
+                    showValidation={showValidation}
                 />
             )
             inputElement = screen.getByTestId(InputTestID)
@@ -51,6 +58,9 @@ describe("Input", () => {
                     onMouseEnter={onHoverInput}
                     onMouseLeave={onBlurInput}
                     onChange={(e) => onChangeInput(e.target.value)}
+                    validateFunc={validateFunc}
+                    showValidation={showValidation}
+                    setIsValid={setIsValid}
                 />
             )
             inputElement = screen.getByTestId(InputTestID)
@@ -75,6 +85,10 @@ describe("Input", () => {
         render(
             <Input
                 label={"login as administrator"}
+                validateFunc={validateFunc}
+                showValidation={showValidation}
+                setIsValid={setIsValid}
+                readOnly
             />
         )
         expect(screen.getByText("login as administrator")).toBeInTheDocument()
