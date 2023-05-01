81a2c680ae078d873229dd5252fe707de722f363
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-01 20:59:52 +0300

    refactor validateInputValue

--- qa-admin/src/main/client/src/utils/validateInputValue/validateInputValue.ts
+++ qa-admin/src/main/client/src/utils/validateInputValue/validateInputValue.ts
@@ -1,6 +1,8 @@
 
-export const validateInputValue = (inputValue: string, maxLength: number = 200): "выглядит хорошо!" | "слишком длинное" | "не может быть пустым" => {
-    if (inputValue.length > maxLength) return "слишком длинное"
+type ValidateInputResult = "выглядит хорошо!" | "слишком длинное" | "не может быть пустым"
+
+export const validateInputValue = (inputValue: HTMLInputElement['value'], maxLength: number = 200): ValidateInputResult => {
+    if(inputValue.length > maxLength) return "слишком длинное"
     if(inputValue.length === 0) return "не может быть пустым"
     return "выглядит хорошо!"
 }
