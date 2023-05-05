32f7a105deb2d5e4196fa9a692669dc82e5682c3
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-05 19:21:28 +0300

    add QuestionService

---
+++ qa-rest/src/main/client/src/services/QuestionService.ts
@@ -0,0 +1,22 @@
+import {$api} from "../http/api";
+import {AxiosResponse} from "axios";
+import {IQuestion} from "../types/IQuestion";
+
+
+export class QuestionService{
+    static fetchAllQuestions():Promise<AxiosResponse<IQuestion[]>> {
+        return $api.get<IQuestion[]>("questions")
+    }
+
+    static fetchQuestionById(id:number):Promise<AxiosResponse<IQuestion>>{
+        return $api.get<IQuestion>(`questions/${id}`)
+    }
+
+    static fetchQuestionByText(text:number):Promise<AxiosResponse<IQuestion>>{
+        return $api.get<IQuestion>(`questions/by-text/${text}`)
+    }
+
+    static fetchQuestionsByCategory(categoryName:string):Promise<AxiosResponse<IQuestion[]>>{
+        return $api.get<IQuestion[]>(`questions/by-category/${categoryName}`)
+    }
+}
