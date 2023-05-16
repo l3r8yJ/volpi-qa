ea0ea95b2395afa28dd57ba17dcd55ef6c405310
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-16 22:53:07 +0300

    fix wrong formatting: remove semicolons

--- qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.test.ts
+++ qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.test.ts
@@ -3,7 +3,7 @@ import {createValidateInputValueFunc} from "./createValidateInputValueFunc";
 import {generateRandomString} from "../generateRandomString/generateRandomString";
 
 describe("validateInputValue", () => {
-    const validateInputValue = createValidateInputValueFunc();
+    const validateInputValue = createValidateInputValueFunc()
     describe("check with default maxLength = 200", () => {
         it.each([
             {str: generateRandomString(Math.floor(Math.random() * 100) + 1), expected: "выглядит хорошо!",},
@@ -15,21 +15,21 @@ describe("validateInputValue", () => {
         ])(
             "string with length $str.length should gives %s",
             ({str, expected}) => {
-                expect(validateInputValue(str)).toBe(expected);
+                expect(validateInputValue(str)).toBe(expected)
             }
         );
     });
 
     describe("check type handles correctly", () => {
         it("should return 'неправильный тип' for all types except string", () => {
-            const undefinedConst = undefined;
-            const numberConst: number = 123;
-            const stringArr: string[] = ["123", "324", "345"];
-            const stringConst: string = "test string";
-            expect(validateInputValue(undefinedConst)).toBe("неправильный тип");
-            expect(validateInputValue(numberConst)).toBe("неправильный тип");
-            expect(validateInputValue(stringArr)).toBe("неправильный тип");
-            expect(validateInputValue(stringConst)).not.toBe("неправильный тип");
+            const undefinedConst = undefined
+            const numberConst: number = 123
+            const stringArr: string[] = ["123", "324", "345"]
+            const stringConst: string = "test string"
+            expect(validateInputValue(undefinedConst)).toBe("неправильный тип")
+            expect(validateInputValue(numberConst)).toBe("неправильный тип")
+            expect(validateInputValue(stringArr)).toBe("неправильный тип")
+            expect(validateInputValue(stringConst)).not.toBe("неправильный тип")
         });
     });
 });
