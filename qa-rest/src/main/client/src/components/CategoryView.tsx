fd6f58913b63a3fdc1959bce4d98ae3ebf6a2d93
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-23 00:11:08 +0300

    add base questions appearance

--- qa-rest/src/main/client/src/components/CategoryView.tsx
+++ qa-rest/src/main/client/src/components/CategoryView.tsx
@@ -1,11 +1,16 @@
 import {FC} from "react"
 import {useAppSelector} from "../hooks/redux";
 
-export const CategoryView:FC = () => {
+export const CategoryView: FC = () => {
     const {currentCategory} = useAppSelector(state => state.view)
+    if (currentCategory === null) return <div>Произошла ошибка при получении категории</div>
     return (
         <div>
-            {currentCategory === null && <div>Произошла ошибка при получении категории</div>}
+            {currentCategory.questions.length > 0
+                ? currentCategory.questions.map(question => (
+                    <div key={question.id}>{question.text}</div>
+                ))
+                : <div>Здесь пока нет вопросов 🧐</div>}
         </div>
     );
 };
