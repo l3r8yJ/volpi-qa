8de4ec64efc2534341932c9a90cf48644de95024
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-22 23:16:58 +0300

    add viewSlice

--- qa-rest/src/main/client/src/store/store.ts
+++ qa-rest/src/main/client/src/store/store.ts
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {questionReducer} from "./reducers/questionSlice"
 import {categoryReducer} from "./reducers/categorySlice";
+import {viewReducer} from "./reducers/viewSlice";
 
 
 export const store = configureStore({
     reducer:{
         question: questionReducer,
-        category: categoryReducer
+        category: categoryReducer,
+        view: viewReducer
     }
 })
 
