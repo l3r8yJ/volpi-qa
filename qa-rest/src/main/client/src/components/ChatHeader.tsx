4518f9be71849903c3098266329272c03d2cda1f
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-22 23:34:08 +0300

    display category view name at the header

--- qa-rest/src/main/client/src/components/ChatHeader.tsx
+++ qa-rest/src/main/client/src/components/ChatHeader.tsx
@@ -1,9 +1,14 @@
 import {FC} from 'react';
+import {useAppSelector} from "../hooks/redux";
 
 export const ChatHeader: FC = () => {
+    const {currentCategory} = useAppSelector(state => state.view)
     return (
         <div className={"p-4 text-xl text-white flex items-center justify-center bg-zinc-800"}>
-            Поступи в ВПИ!
+            {currentCategory === null
+                ? <h1>Поступи в ВПИ!</h1>
+                : <h1>{currentCategory.name}</h1>
+            }
         </div>
     );
 }
