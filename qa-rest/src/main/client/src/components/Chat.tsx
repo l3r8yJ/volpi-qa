2d40f42476383d9b7a50d6adaf022637b2c9e8ab
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-22 22:11:36 +0300

    fix missed prop

--- qa-rest/src/main/client/src/components/Chat.tsx
+++ qa-rest/src/main/client/src/components/Chat.tsx
@@ -21,7 +21,7 @@ export const Chat: FC = () => {
                 </div>
                 <div className={"space-y-4"}>
                     {categories.map((category) => (
-                        <Category key={category.id}/>
+                        <Category key={category.id} category={category}/>
                     ))}
                 </div>
             </div>
