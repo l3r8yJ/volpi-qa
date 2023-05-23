53d57bd8e3c72df0f72d7cda53884aa968560213
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-23 12:25:27 +0300

    update styles

--- qa-rest/src/main/client/src/components/Question.tsx
+++ qa-rest/src/main/client/src/components/Question.tsx
@@ -5,12 +5,16 @@ import {ChevronDownIcon} from "@heroicons/react/24/outline";
 
 export const Question: FC<Omit<IQuestion, "id" | "category">> = ({text, answer}) => {
     return (
-        <Disclosure >
-            <Disclosure.Button className={"bg-blue-100 rounded-lg p-2 flex justify-between text-blue-900"}>
-                <span className={"font-semibold"}>{text}</span>
-                <ChevronDownIcon className={"w-4 h-4 ui-open:rotate-180 ui-not-open:rotate-0"}/>
-            </Disclosure.Button>
-            <Disclosure.Panel>{answer}</Disclosure.Panel>
+        <Disclosure>
+            <div
+                className={"w-full flex justify-center flex-col"}>
+                <Disclosure.Button className={"p-2 flex justify-between items-center bg-neutral-100 hover:bg-neutral-200 rounded-lg"}>
+                    <span className={"font-medium"}>{text}</span>
+                    <ChevronDownIcon
+                        className={"w-4 h-4 ui-open:rotate-180 ui-not-open:rotate-0 ui-open:transform ui-open:duration-150"}/>
+                </Disclosure.Button>
+                <Disclosure.Panel className={"px-2 pt-2 text-neutral-900/60 text-sm"}>{answer}</Disclosure.Panel>
+            </div>
         </Disclosure>
     );
 };
