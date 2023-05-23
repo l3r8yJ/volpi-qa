8af25d93c4f53b6b3325fb71b4937b809ae2d738
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-23 22:58:40 +0300

    add check for not found result

--- qa-rest/src/main/client/src/components/TextInputForm.tsx
+++ qa-rest/src/main/client/src/components/TextInputForm.tsx
@@ -27,18 +27,24 @@ export const TextInputForm: FC = () => {
         <div className={"bg-white"}>
             <Combobox value={selectedValue} onChange={setSelectedValue}>
                 <Combobox.Options
-                    className={"max-h-36 overflow-y-auto rounded-lg overflow-hidden border py-1.5 mb-3 mx-2 shadow-lg shadow-neutral-500/50"}>
-                    {filteredQuestions.map((person) => (
-                        <Combobox.Option
-                            key={person}
-                            value={person}
-                            as={Fragment}
-                        >
-                            {({active, selected}) => (
-                                <li
-                                    className={`cursor-pointer select-none flex p-2 pl-10 pr-2 relative
+                    className={"max-h-36 overflow-y-auto rounded-lg overflow-hidden border py-1.5 mb-3 mx-2 shadow-lg shadow-neutral-500/50"}
+                >
+                    {filteredQuestions.length === 0 && query !== '' ? (
+                        <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                            Ничего не найдено.
+                        </div>
+                    ) : (
+                        filteredQuestions.map((person) => (
+                            <Combobox.Option
+                                key={person}
+                                value={person}
+                                as={Fragment}
+                            >
+                                {({active, selected}) => (
+                                    <li
+                                        className={`cursor-pointer select-none flex p-2 pl-10 pr-2 relative
                                     ${active ? 'bg-blue-600 text-white ' : ' text-gray-900'}`}
-                                >
+                                    >
                                     <span>
                                         {selected ? (
                                                 <span className={"font-semibold"}>{person}</span>
@@ -47,15 +53,15 @@ export const TextInputForm: FC = () => {
                                                 <span>{person}</span>
                                             )}
                                     </span>
-                                    <span
-                                        className={"absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600 ui-active:text-white"}
-                                    >
+                                        <span
+                                            className={"absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600 ui-active:text-white"}
+                                        >
                                             <CheckIcon className="h-5 w-5 hidden ui-selected:block" aria-hidden="true"/>
                                         </span>
-                                </li>
-                            )}
-                        </Combobox.Option>
-                    ))}
+                                    </li>
+                                )}
+                            </Combobox.Option>
+                        )))}
                 </Combobox.Options>
                 <form className={"flex py-4 px-2 bg-zinc-50 border-t"}>
                     <div
