a29dedfb82b165691fce1f7615be9bc9242c110c
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-23 11:13:04 +0300

    add disclosure to question component

--- qa-rest/src/main/client/src/components/Question.tsx
+++ qa-rest/src/main/client/src/components/Question.tsx
@@ -1,11 +1,16 @@
 import {FC} from "react"
 import {IQuestion} from "../types/IQuestion";
+import {Disclosure} from "@headlessui/react";
+import {ChevronDownIcon} from "@heroicons/react/24/outline";
 
 export const Question: FC<Omit<IQuestion, "id" | "category">> = ({text, answer}) => {
     return (
-        <div className={"bg-neutral-100 rounded-lg p-2"}>
-            <div className={"font-bold"}>{text}</div>
-            <div>{answer}</div>
-        </div>
+        <Disclosure >
+            <Disclosure.Button className={"bg-blue-100 rounded-lg p-2 flex justify-between text-blue-900"}>
+                <span className={"font-semibold"}>{text}</span>
+                <ChevronDownIcon className={"w-4 h-4 ui-open:rotate-180 ui-not-open:rotate-0"}/>
+            </Disclosure.Button>
+            <Disclosure.Panel>{answer}</Disclosure.Panel>
+        </Disclosure>
     );
 };
