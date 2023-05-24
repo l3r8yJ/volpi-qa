b65ef56a362f830ede383f910385ba2f67f24982
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-24 13:40:23 +0300

    fix bug: now displayed header value depends on the screen type

--- qa-rest/src/main/client/src/components/ChatHeader.tsx
+++ qa-rest/src/main/client/src/components/ChatHeader.tsx
@@ -3,13 +3,14 @@ import {useAppDispatch, useAppSelector} from "../hooks/redux";
 import {ArrowLeftIcon, XMarkIcon} from "@heroicons/react/24/outline";
 import {setIsOpen} from "../store/reducers/modalSlice";
 import {setCurrentView} from "../store/reducers/viewSlice";
+import {ENROLL_TO_VPI} from "../constants/vpi";
 
 export const ChatHeader: FC = () => {
-    const {viewHeaderText} = useAppSelector(state => state.view)
+    const {viewHeaderText, currentView} = useAppSelector(state => state.view)
     const dispatch = useAppDispatch()
     return (
         <div className={"p-4 text-xl text-white flex items-center justify-between bg-zinc-800"}>
-            {viewHeaderText !== null
+            {currentView !== "default"
                 ? <button
                     className={"text-white hover:text-neutral-300"}
                     onClick={() => dispatch(setCurrentView("default"))}
