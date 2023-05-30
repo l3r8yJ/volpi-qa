aaa97c4a42b3789c8f8c864e0b67d21e71aac264
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 18:04:14 +0300

    add ability to hide header in main layout

--- qa-admin/src/main/client/src/components/layouts/MainLayout.tsx
+++ qa-admin/src/main/client/src/components/layouts/MainLayout.tsx
@@ -3,13 +3,14 @@ import {Header} from "../Header";
 
 interface MainLayoutProps{
     children:ReactNode
+    isHeader?: boolean
 }
 
-export const MainLayout: FC<MainLayoutProps> = ({children}) => {
+export const MainLayout: FC<MainLayoutProps> = ({children, isHeader = true}) => {
     return (
         <div className={"text-white min-h-screen bg-neutral-900"}>
-            <Header/>
-            <main className={"mx-5 lg:container lg:mx-auto mt-4"}>
+            {isHeader && <Header/> }
+            <main className={`mx-5 lg:container lg:mx-auto ${isHeader && "mt-4"}`}>
                 {children}
             </main>
         </div>
