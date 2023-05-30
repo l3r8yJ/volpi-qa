d4e52a2852b3409d67086477cf48500186ca0df5
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 17:51:32 +0300

    add not authorized routes const

--- qa-admin/src/main/client/src/constants/routes.tsx
+++ qa-admin/src/main/client/src/constants/routes.tsx
@@ -1,4 +1,5 @@
 import {lazy, ReactElement} from "react";
+import {Login} from "../pages/Login";
 const Categories = lazy(() => import("../pages/Categories"))
 const CategoryName = lazy(() => import("../pages/CategoryName"))
 
@@ -6,6 +7,10 @@ interface IRoute {
     path: string
     element: ReactElement
 }
+
+export const notAuthorizedRoutes: IRoute[] = [
+    {path: "/login", element: <Login/>}
+]
 
 export const authorizedRoutes: IRoute[] = [
     {path: "/", element: <Categories/>},
