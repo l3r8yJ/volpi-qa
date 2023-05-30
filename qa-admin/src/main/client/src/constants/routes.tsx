f57990bf1e629e834d9a280a071bea5f59f2c31a
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 17:56:10 +0300

    add redirecting from wrong urls to safety

--- qa-admin/src/main/client/src/constants/routes.tsx
+++ qa-admin/src/main/client/src/constants/routes.tsx
@@ -1,5 +1,6 @@
 import {lazy, ReactElement} from "react";
 import {Login} from "../pages/Login";
+import {Navigate} from "react-router-dom";
 const Categories = lazy(() => import("../pages/Categories"))
 const CategoryName = lazy(() => import("../pages/CategoryName"))
 
@@ -9,10 +10,12 @@ interface IRoute {
 }
 
 export const notAuthorizedRoutes: IRoute[] = [
-    {path: "/login", element: <Login/>}
+    {path: "/login", element: <Login/>},
+    {path: "*", element: <Navigate to={"/login"}/>}
 ]
 
 export const authorizedRoutes: IRoute[] = [
     {path: "/", element: <Categories/>},
-    {path: "/categories/:name", element: <CategoryName/>}
+    {path: "/categories/:name", element: <CategoryName/>},
+    {path: "*", element: <Navigate to={"/"}/>}
 ]
