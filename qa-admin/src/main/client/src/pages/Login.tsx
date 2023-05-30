020f4803b3533ef4b2d20c18896ce3859832d37e
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 19:56:13 +0300

    base login page ui

--- qa-admin/src/main/client/src/pages/Login.tsx
+++ qa-admin/src/main/client/src/pages/Login.tsx
@@ -1,14 +1,16 @@
 import {FC} from "react"
 import {MainLayout} from "../components/layouts/MainLayout";
 import {ValidatedInput} from "../components/UI/ValidatedInput/ValidatedInput";
+import {LoginForm} from "../components/LoginForm";
 
 const Login: FC = () => {
     return (
         <MainLayout isHeader={false}>
             <div className={"min-h-screen flex flex-col justify-center items-center"}>
-                <h1 className={"text-3xl font-semibold"}>
+                <h1 className={"text-4xl font-semibold mb-10"}>
                     Volpi-QA <span className={"text-indigo-600"}>admin</span>
                 </h1>
+                <LoginForm/>
             </div>
         </MainLayout>
     );
