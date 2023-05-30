2771d7ab6635af217eec222d7ac07e0486d3ef9c
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 17:53:27 +0300

    add test dynamic auth routing

--- qa-admin/src/main/client/src/components/AppRouter.tsx
+++ qa-admin/src/main/client/src/components/AppRouter.tsx
@@ -1,12 +1,14 @@
 import React, {FC, Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
-import {authorizedRoutes} from "../constants/routes";
+import {authorizedRoutes, notAuthorizedRoutes} from "../constants/routes";
 import {SimpleLayout} from "./layouts/SimpleLayout";
 
 export const AppRouter: FC = () => {
+    const isAuth = false
+    let routes = isAuth ? authorizedRoutes : notAuthorizedRoutes
     return (
         <Routes>
-            {authorizedRoutes.map((route) => (
+            {routes.map((route) => (
                 <Route
                     path={route.path}
                     element={
