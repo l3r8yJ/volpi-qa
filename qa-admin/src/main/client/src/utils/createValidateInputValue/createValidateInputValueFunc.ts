73814a59a0bc74d8e1e3237b3cb0984cc082a36b
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 18:40:20 +0300

    fully typed createValidateInputFunc util

--- qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
+++ qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
@@ -2,11 +2,13 @@ import {InputHTMLAttributes} from "react";
 
 export type ValidateInputResult = "выглядит хорошо!" | "слишком длинное" | "не может быть пустым" | "неправильный тип"
 
-export const createValidateInputValueFunc = (maxLength: number = 200) => {
+export type ValidateInputResulFunc = (inputValue: InputHTMLAttributes<HTMLInputElement>['value']) => ValidateInputResult
+
+export const createValidateInputValueFunc = (maxLength: number = 200): ValidateInputResulFunc => {
     return (inputValue: InputHTMLAttributes<HTMLInputElement>['value']): ValidateInputResult => {
-        if(typeof inputValue !== "string") return "неправильный тип"
-        if(inputValue.length > maxLength) return "слишком длинное"
-        if(inputValue.length === 0) return "не может быть пустым"
+        if (typeof inputValue !== "string") return "неправильный тип"
+        if (inputValue.length > maxLength) return "слишком длинное"
+        if (inputValue.length === 0) return "не может быть пустым"
         return "выглядит хорошо!"
     }
 }
