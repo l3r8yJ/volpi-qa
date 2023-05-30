42f98e96229778784f9bd376de52818a0b969815
Andrew-Sem <avvyayvayva@gmail.com>
2023-05-30 18:18:41 +0300

    add base logo to login page

--- qa-admin/src/main/client/src/pages/Login.tsx
+++ qa-admin/src/main/client/src/pages/Login.tsx
@@ -1,10 +1,15 @@
 import {FC} from "react"
 import {MainLayout} from "../components/layouts/MainLayout";
+import {ValidatedInput} from "../components/UI/ValidatedInput/ValidatedInput";
 
 const Login: FC = () => {
     return (
         <MainLayout isHeader={false}>
-            login
+            <div className={"min-h-screen flex flex-col justify-center items-center"}>
+                <h1 className={"text-3xl font-semibold"}>
+                    Volpi-QA <span className={"text-indigo-600"}>admin</span>
+                </h1>
+            </div>
         </MainLayout>
     );
 };
