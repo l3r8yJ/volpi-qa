361353395aafbd55c92facb575d0a644d70aa964
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-05 21:44:48 +0300

    add ability to toggle password visibility

--- qa-admin/src/main/client/src/components/LoginForm.tsx
+++ qa-admin/src/main/client/src/components/LoginForm.tsx
@@ -5,7 +5,7 @@ import {PrimaryButton} from "./UI/PrimaryButton/PrimaryButton";
 import {BoltIcon} from "@heroicons/react/24/solid";
 import {useAppDispatch, useAppSelector} from "../hooks/redux";
 import {auth} from "../store/actions/authAction";
-import {ExclamationTriangleIcon} from "@heroicons/react/24/outline";
+import {ExclamationTriangleIcon, EyeIcon, EyeSlashIcon} from "@heroicons/react/24/outline";
 import {Loader} from "./UI/Loader";
 import {LoaderSize} from "../utils/getLoaderSizeByName";
 
@@ -17,12 +17,16 @@ export const LoginForm: FC = () => {
     const [username, setUsername] = useState("")
     const [isPasswordValid, setIsPasswordValid] = useState(false)
     const [password, setPassword] = useState("")
+    const [isShowPassword, setIsShowPassword] = useState(false)
     const {loading} = useAppSelector(state => state.auth)
     const dispatch = useAppDispatch()
     const formHandler = (e: FormEvent) => {
         e.preventDefault()
         if (!isUsernameValid || !isPasswordValid) return setShowValidation(true)
         dispatch(auth({username, password}))
+    }
+    const toggleIsShowPassword = () => {
+        setIsShowPassword(prev => !prev)
     }
     return (
         <form className={"w-64 p-4 border border-base/50 rounded-lg flex flex-col"} onSubmit={formHandler}>
@@ -40,15 +44,29 @@ export const LoginForm: FC = () => {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     label={"Логин"}
+                    type={"text"}
                 />
-                <ValidatedInput
-                    validateFunc={validateInputValue}
-                    setIsValid={setIsPasswordValid}
-                    showValidation={showValidation}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    label={"Пароль"}
-                />
+                <div className={"relative"}>
+                    <ValidatedInput
+                        validateFunc={validateInputValue}
+                        setIsValid={setIsPasswordValid}
+                        showValidation={showValidation}
+                        value={password}
+                        type={isShowPassword ? "text" : "password"}
+                        onChange={(e) => setPassword(e.target.value)}
+                        label={"Пароль"}
+                        className={"pr-10"}
+                    />
+                    <div
+                        className={"absolute right-2 top-1/2 h-6 w-6 bg-secondary cursor-pointer hover:text-contrastHov"}
+                        onClick={toggleIsShowPassword}
+                    >
+                        {isShowPassword
+                            ? <EyeSlashIcon className={"w-5 h-5"}/>
+                            : <EyeIcon className={"w-5 h-5"}/>
+                        }
+                    </div>
+                </div>
             </div>
             <PrimaryButton type={"submit"} className={"flex items-center justify-center mt-8 min-h-[40px]"}>
                 {loading === "pending"
