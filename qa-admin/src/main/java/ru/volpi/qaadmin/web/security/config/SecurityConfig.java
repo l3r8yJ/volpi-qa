5db92126dc2323ee2177b0c26549d302077f4c21
Ivanchuck Ivan <clicker.heroes.acg@gmail.com>
2023-06-05 13:04:55 +0300

    Add cross-origin fix

--- qa-admin/src/main/java/ru/volpi/qaadmin/web/security/config/SecurityConfig.java
+++ qa-admin/src/main/java/ru/volpi/qaadmin/web/security/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package ru.volpi.qaadmin.web.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +29,11 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http, @Qualifier("corsConfigurer") final WebMvcConfigurer configurer) throws Exception {
         http
             .csrf()
             .disable()
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests()
             .requestMatchers("/api/v1/admin/auth/**", "/api/v1/admin/register/**")
             .permitAll()
@@ -37,5 +46,16 @@ public class SecurityConfig {
             .authenticationProvider(this.authenticationProvider)
             .addFilterBefore(this.jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
