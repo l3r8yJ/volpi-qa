a27fed4c4210692fad718c5ae5cb855d095a1998
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-06 23:37:44 +0300

    took ui of the error out of the form

--- qa-admin/src/main/client/src/components/auth/LoginForm.tsx
+++ qa-admin/src/main/client/src/components/auth/LoginForm.tsx
@@ -8,7 +8,6 @@ import {auth} from "../../store/actions/authAction";
 import {ExclamationTriangleIcon} from "@heroicons/react/24/outline";
 import {Loader} from "../UI/Loader";
 import {LoaderSize} from "../../utils/getLoaderSizeByName";
-import {getErrorDescription} from "../../utils/getErrorDescription";
 import {clearTheError} from "../../store/reducers/authSlice";
 
 const validateInputValue = createValidateInputValueFunc()
@@ -31,45 +30,46 @@ export const LoginForm: FC = () => {
     }
 
     return (
-        <form className={"w-80 px-4 py-6 border border-base/50 rounded-lg flex flex-col"} onSubmit={formHandler}>
+        <>
             {loading === "failed" && error &&
-                <div className={"text-danger flex items-center justify-center py-3 gap-2"}>
+                <div className={"text-danger flex items-center justify-center mb-3 gap-2"}>
                     <ExclamationTriangleIcon className={"w-5 h-5"}/>
-                    <span>{getErrorDescription(error)}</span>
+                    <span>{error}</span>
                 </div>
             }
-            <div className={"space-y-4 flex flex-col"}>
-                <ValidatedInput
-                    validateFunc={validateInputValue}
-                    setIsValid={setIsUsernameValid}
-                    showValidation={showValidation}
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    label={"Логин"}
-                    type={"text"}
-                    onClick={inputClickHandler}
-                />
-                <ValidatedInput
-                    validateFunc={validateInputValue}
-                    setIsValid={setIsPasswordValid}
-                    showValidation={showValidation}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    label={"Пароль"}
-                    isPassword={true}
-                    onClick={inputClickHandler}
-                />
-            </div>
-            <PrimaryButton type={"submit"} className={"flex items-center justify-center mt-8 min-h-[40px]"}>
-                {loading === "pending"
-                    ? <Loader size={LoaderSize.small}/>
-                    : <>
-                        <BoltIcon className={"w-4 h-4"}/>
-                        <span className={"ml-2"}>Войти</span>
-                    </>
-                }
-
-            </PrimaryButton>
-        </form>
+            <form className={"w-80 px-4 py-6 border border-base/50 rounded-lg flex flex-col"} onSubmit={formHandler}>
+                <div className={"space-y-4 flex flex-col"}>
+                    <ValidatedInput
+                        validateFunc={validateInputValue}
+                        setIsValid={setIsUsernameValid}
+                        showValidation={showValidation}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        label={"Логин"}
+                        type={"text"}
+                        onClick={inputClickHandler}
+                    />
+                    <ValidatedInput
+                        validateFunc={validateInputValue}
+                        setIsValid={setIsPasswordValid}
+                        showValidation={showValidation}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        label={"Пароль"}
+                        isPassword={true}
+                        onClick={inputClickHandler}
+                    />
+                </div>
+                <PrimaryButton type={"submit"} className={"flex items-center justify-center mt-8 min-h-[40px]"}>
+                    {loading === "pending"
+                        ? <Loader size={LoaderSize.small}/>
+                        : <>
+                            <BoltIcon className={"w-4 h-4"}/>
+                            <span className={"ml-2"}>Войти</span>
+                        </>
+                    }
+                </PrimaryButton>
+            </form>
+        </>
     );
 };
