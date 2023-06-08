14c0cdbfeae4e549813d9715b1be6f746de93805
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-08 17:25:01 +0300

    add handling 403 code to api

--- qa-admin/src/main/client/src/http/api.ts
+++ qa-admin/src/main/client/src/http/api.ts
@@ -1,5 +1,7 @@
 import axios, {AxiosRequestHeaders} from "axios";
 import {API_URL} from "../constants/api";
+import {store} from "../store/store";
+import {setTokenInvalid, signOut} from "../store/reducers/authSlice";
 
 
 export const $api = axios.create({
@@ -14,3 +16,17 @@ $api.interceptors.request.use((config) => {
     if(token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
+
+$api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 403) {
+            store.dispatch(setTokenInvalid());
+            store.dispatch(signOut())
+        }
+        return Promise.reject(error);
+    }
+);
+
