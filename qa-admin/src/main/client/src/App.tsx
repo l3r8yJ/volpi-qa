2b35264e2b0a312f118a4d45fefd4df02c84ec79
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-08 18:00:21 +0300

    import res interceptors directly to app

--- qa-admin/src/main/client/src/App.tsx
+++ qa-admin/src/main/client/src/App.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router} from "react-router-dom"
 import {AppRouter} from "./components/AppRouter";
+import "./http/apiResInterceptors"
 
 function App() {
     return (
--- qa-admin/src/main/client/src/http/api.ts
+++ qa-admin/src/main/client/src/http/api.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 import {API_URL} from "../constants/api";
+import {AxiosRequestHeaders} from "axios/index";
 
 
 export const $api = axios.create({
     baseURL: API_URL
 })
+
+$api.interceptors.request.use((config) => {
+    if (config.headers === undefined) {
+        config.headers = {} as AxiosRequestHeaders;
+    }
+    const token = localStorage.getItem("token")
+    if(token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
+
 
 
 
