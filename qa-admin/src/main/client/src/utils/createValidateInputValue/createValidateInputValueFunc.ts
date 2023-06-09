e1771ac0ca3eb293b2c130751cc7f4f18b6d863b
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-09 23:58:03 +0300

    add ban words option to validate func, refactoring

--- qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
+++ qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
@@ -1,14 +1,25 @@
 import {InputHTMLAttributes} from "react";
 
-export type ValidateInputResult = "выглядит хорошо!" | "слишком длинное" | "не может быть пустым" | "неправильный тип"
+export type ValidateInputResult =
+    "выглядит хорошо!"
+    | "слишком длинное"
+    | "не может быть пустым"
+    | "неправильный тип"
+    | "уже существует"
 
 export type ValidateInputResulFunc = (inputValue: InputHTMLAttributes<HTMLInputElement>['value']) => ValidateInputResult
 
-export const createValidateInputValueFunc = (maxLength: number = 200): ValidateInputResulFunc => {
+type ValidateOptions = {
+    maxLength?: number
+    banWords?: string[]
+}
+
+export const createValidateInputValueFunc = ({maxLength = 200, banWords}: ValidateOptions): ValidateInputResulFunc => {
     return (inputValue: InputHTMLAttributes<HTMLInputElement>['value']): ValidateInputResult => {
         if (typeof inputValue !== "string") return "неправильный тип"
         if (inputValue.length > maxLength) return "слишком длинное"
         if (inputValue.trim().length === 0) return "не может быть пустым"
+        if (banWords?.includes(inputValue)) return "уже существует"
         return "выглядит хорошо!"
     }
 }
