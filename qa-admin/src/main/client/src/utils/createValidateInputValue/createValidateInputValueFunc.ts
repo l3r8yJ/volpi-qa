1b4b3995182333e714a91db5efabf1a2c1c76146
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-10 00:02:11 +0300

    options now are optional

--- qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
+++ qa-admin/src/main/client/src/utils/createValidateInputValue/createValidateInputValueFunc.ts
@@ -14,10 +14,11 @@ type ValidateOptions = {
     banWords?: string[]
 }
 
-export const createValidateInputValueFunc = ({maxLength = 200, banWords}: ValidateOptions): ValidateInputResulFunc => {
+export const createValidateInputValueFunc = (options?: ValidateOptions): ValidateInputResulFunc => {
+    const {maxLength, banWords} = options ?? {}
     return (inputValue: InputHTMLAttributes<HTMLInputElement>['value']): ValidateInputResult => {
         if (typeof inputValue !== "string") return "неправильный тип"
-        if (inputValue.length > maxLength) return "слишком длинное"
+        if (maxLength && inputValue.length > maxLength) return "слишком длинное"
         if (inputValue.trim().length === 0) return "не может быть пустым"
         if (banWords?.includes(inputValue)) return "уже существует"
         return "выглядит хорошо!"
