47e26a9298db6d408bb0eba006e591fa4968bb90
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-15 22:31:19 +0300

    update font bold

--- qa-rest/src/main/client/src/utils/parseLinks.ts
+++ qa-rest/src/main/client/src/utils/parseLinks.ts
@@ -3,6 +3,6 @@ export const parseLinks = (text: string) => {
     return text.replace(urlRegex, (url, domainAndPath) => {
         const fullUrl = url.startsWith('http') ? url : `http://${url}`;
         if(domainAndPath.length > 30) domainAndPath = domainAndPath.slice(0, 27) + "..."
-        return `<a href='${fullUrl}' class="text-indigo-600 break-words hover:text-indigo-500 text-base bg-indigo-100 p-0.5 rounded">${domainAndPath}</a>`;
+        return `<a href='${fullUrl}' class="text-indigo-600 break-words hover:text-indigo-500 font-medium text-base bg-indigo-100 p-0.5 rounded">${domainAndPath}</a>`;
     });
 }
