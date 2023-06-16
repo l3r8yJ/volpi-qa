1affc8f801761b29a9f7842d28ccb3b60fbb1cd2
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-16 11:38:37 +0300

    fix incorrect display big words

--- qa-rest/src/main/client/src/components/Question.tsx
+++ qa-rest/src/main/client/src/components/Question.tsx
@@ -12,14 +12,14 @@ export const Question: FC<Omit<IQuestion, "id" | "category">> = ({text, answer})
                 <Disclosure.Button
                     className={"p-2 flex justify-between items-center bg-neutral-100 hover:bg-neutral-200 rounded-lg"}>
                     <div
-                        className={"font-medium text-left w-5/6"}
+                        className={"font-medium text-left w-5/6 break-words"}
                         dangerouslySetInnerHTML={{__html: parseLinks(text)}}
                     ></div>
                     <ChevronDownIcon
                         className={"w-4 h-4 ml-2 ui-open:rotate-180 ui-not-open:rotate-0 ui-open:transform ui-open:duration-150"}/>
                 </Disclosure.Button>
                 <Disclosure.Panel
-                    className={"px-2 pt-2 text-neutral-900/60 text-sm"}
+                    className={"px-2 pt-2 text-neutral-900/60 text-sm break-words"}
                     dangerouslySetInnerHTML={{__html: parseLinks(answer)}}
                 ></Disclosure.Panel>
             </div>
