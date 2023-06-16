685949786986281fabac64856a803e20cc2f9176
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-16 20:54:10 +0300

    now parseLinks inclume more domains, default protocol replaces to https

--- qa-admin/src/main/client/src/utils/parseLinks/parseLinks.ts
+++ qa-admin/src/main/client/src/utils/parseLinks/parseLinks.ts
@@ -1,8 +1,9 @@
 export const parseLinks = (text: string) => {
-    const urlRegex = /(?:https?:\/\/)?(?:www\.)?([^\s]+(\.ru|\.com)[^\s]*)/g;
+    const urlRegex = /(?:https?:\/\/)?(?:www\.)?([^\s]+(\.ru|\.com|\.ai|\.рф|\.app|\.net|\.org|\.io|\.gov|\.edu|\.mil|\.int)[^\s]*)/g;
     return text.replace(urlRegex, (url:string, domainAndPath:string) => {
-        const fullUrl = url.startsWith('http') ? url : `http://${url}`;
+        const fullUrl = url.startsWith("http") ? url : `https://${url}`;
         if(domainAndPath.length > 40) domainAndPath = domainAndPath.slice(0, 40) + "..."
         return `<a href='${fullUrl}' class="text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded">${domainAndPath}</a>`;
     });
 }
+
