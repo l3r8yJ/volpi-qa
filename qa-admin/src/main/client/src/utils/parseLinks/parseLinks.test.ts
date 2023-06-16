834e43106d410275fadc67cd0952d91d05b62376
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-16 20:54:19 +0300

    update styles

--- qa-admin/src/main/client/src/utils/parseLinks/parseLinks.test.ts
+++ qa-admin/src/main/client/src/utils/parseLinks/parseLinks.test.ts
@@ -1,15 +1,17 @@
 import {describe, it, expect} from "vitest"
 import {parseLinks} from "./parseLinks";
 
+const aClasses = '\"text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded\"'
+
 describe("parseLinks", () => {
     it.each([
         [
             "losos karas https://volpi.ru/sveden/education",
-            "losos karas <a href='https://volpi.ru/sveden/education' class=\"text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded\">volpi.ru/sveden/education</a>"
+            `losos karas <a href='https://volpi.ru/sveden/education' class=${aClasses}>volpi.ru/sveden/education</a>`
         ],
         [
             "https://volpi.ru/sveden/education losos karas",
-            "<a href='https://volpi.ru/sveden/education' class=\"text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded\">volpi.ru/sveden/education</a> losos karas"
+            `<a href='https://volpi.ru/sveden/education' class=${aClasses}>volpi.ru/sveden/education</a> losos karas`
         ],
         [
             "https://volpi.ru/losos/karas",
@@ -21,7 +23,11 @@ describe("parseLinks", () => {
         ],
         [
             "some big link vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd",
-            "some big link <a href='http://vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd\' class=\"text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded\">vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfj...</a>"
+            "some big link <a href='https://vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd\' class=\"text-link hover:text-linkHov text-base bg-secondary p-0.5 border border-border/40 rounded\">vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfj...</a>"
+        ],
+        [
+            "andrew-semyonov.vercel.app",
+            `<a href='https://andrew-semyonov.vercel.app' class=${aClasses}>andrew-semyonov.vercel.app</a>`
         ]
     ])("text: '%s' should return '%s'", (text: string, expected: string) => {
         expect(parseLinks(text)).toBe(expected)
