4281658b4e4ce0add1fe51762ddf57866cc4595a
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-29 20:29:25 +0300

    add page for asked questions

---
+++ qa-admin/src/main/client/src/pages/AskedQuestions.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react"
+import {MainLayout} from "../components/layouts/MainLayout";
+
+export const AskedQuestions:FC = () => {
+    return (
+        <MainLayout>
+
+        </MainLayout>
+    );
+};
