ce5d8ed03bcb1f0200d96a4057570fc25d6eb4d2
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-30 21:50:26 +0300

    rewrite colors to hsl vars

--- qa-admin/src/main/client/tailwind.config.cjs
+++ qa-admin/src/main/client/tailwind.config.cjs
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import * as colors from 'tailwindcss/colors'
 
 module.exports = {
     content: [
@@ -9,23 +8,30 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: colors.indigo["700"],
-                primaryHov: colors.indigo["800"],
-                secondary: colors.neutral["900"],
-                secondaryEven: colors.neutral["800"],
-                pale: colors.neutral["400"],
-                paleHov: colors.neutral["200"],
-                danger: colors.red["600"],
-                dangerHov: colors.red["700"],
-                safe: colors.green["600"],
-                safeHov: colors.green["700"],
-                border: colors.neutral["500"],
-                contrast: colors.neutral["100"],
-                contrastHov: colors.neutral["400"],
-                shadow: colors.black,
-                link: colors.indigo[400],
-                linkHov: colors.indigo[500],
-                skeleton: colors.neutral[500]
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))"
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))"
+                },
+                safe: {
+                    DEFAULT: "hsl(var(--safe))",
+                    foreground: "hsl(var(--safe-foreground))"
+                },
+                danger: {
+                    DEFAULT: "hsl(var(--danger))",
+                    foreground: "hsl(var(--danger-foreground))"
+                },
+                pale: {
+                    DEFAULT: "hsl(var(--pale))",
+                    foreground: "hsl(var(--pale-foreground))"
+                },
+                border: "hsl(var(--border))",
+                link: "hsl(var(--link))",
+                shadow: "hsl(var(--shadow))",
+                skeleton: "hsl(var(--skeleton))",
             },
         },
     },
