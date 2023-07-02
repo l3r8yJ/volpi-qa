436e773b98c77c3aba354dc1f8e65aa1920f14f3
Ivanchuck Ivan <clicker.heroes.acg@gmail.com>
2023-07-02 22:18:09 +0300

    Formatting

--- qa-rest/src/main/java/ru/volpi/qarest/domain/question/UnknownQuestion.java
+++ qa-rest/src/main/java/ru/volpi/qarest/domain/question/UnknownQuestion.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
