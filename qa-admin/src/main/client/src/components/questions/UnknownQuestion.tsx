a6b7fd466fbec7f8147ce3d9f68495fe966cdf31
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-02 18:49:11 +0300

    base ui for deleting unknown questions

--- qa-admin/src/main/client/src/components/questions/UnknownQuestion.tsx
+++ qa-admin/src/main/client/src/components/questions/UnknownQuestion.tsx
@@ -2,18 +2,47 @@ import {FC} from "react"
 import {UnknownQuestion as UnknownQuestionType} from "../../types/IQuestion";
 import {SecondaryButton} from "../UI/SecondaryButton";
 import {UnknownQuestionForm} from "./UnknownQuestionForm";
+import {Popup} from "../UI/Popup";
+import {PrimaryButton} from "../UI/PrimaryButton/PrimaryButton";
+import {NoSymbolIcon} from "@heroicons/react/20/solid";
+import {XMarkIcon} from "@heroicons/react/24/outline";
 
 interface UnknownQuestionProps {
     question: UnknownQuestionType
 }
 
 export const UnknownQuestion: FC<UnknownQuestionProps> = ({question}) => {
+    const deleteHandler = () => {
+
+    }
     return (
         <div className={"flex flex-col p-4 gap-y-4 items-start"}>
             <div>{question.text}</div>
             <div className={"flex gap-x-2"}>
                 <UnknownQuestionForm defaultQuestionText={question.text} questionId={question.id}/>
-                <SecondaryButton variant={"danger"} className={"w-auto"}>Удалить</SecondaryButton>
+                <Popup
+                    ButtonElement={
+                        <SecondaryButton variant={"danger"} className={"w-auto"}>Удалить</SecondaryButton>
+                    }
+                    position={"right"}
+                    optionButtons={[
+                        <PrimaryButton
+                            className={"flex items-center justify-center gap-x-1"}
+                            variant={"safe"}
+                        >
+                            <NoSymbolIcon className={"w-5 h-5"}/>
+                            <span>Отменить</span>
+                        </PrimaryButton>,
+                        <SecondaryButton
+                            className={"flex items-center justify-center gap-x-1"}
+                            variant={"danger"}
+                        >
+                            <XMarkIcon className={"w-5 h-5"}/>
+                            <span>Удалить</span>
+                        </SecondaryButton>]
+                    }
+                    title={"Вы действительно хотите удалить вопрос?"}
+                />
             </div>
         </div>
     );
