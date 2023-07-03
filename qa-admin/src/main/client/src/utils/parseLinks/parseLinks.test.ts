fb98497310cebfc86280197da8b8c9218f7522dc
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-03 18:50:54 +0300

    update tests

--- qa-admin/src/main/client/src/utils/parseLinks/parseLinks.test.ts
+++ qa-admin/src/main/client/src/utils/parseLinks/parseLinks.test.ts
@@ -7,27 +7,28 @@ describe("parseLinks", () => {
     it.each([
         [
             "losos karas https://volpi.ru/sveden/education",
-            `losos karas <a href='https://volpi.ru/sveden/education' class=${aClasses}>volpi.ru/sveden/education</a>`
+            `losos karas <a href="https://volpi.ru/sveden/education" class=${aClasses}>volpi.ru/sveden/education</a>`
         ],
         [
             "https://volpi.ru/sveden/education losos karas",
-            `<a href='https://volpi.ru/sveden/education' class=${aClasses}>volpi.ru/sveden/education</a> losos karas`
+            `<a href="https://volpi.ru/sveden/education" class=${aClasses}>volpi.ru/sveden/education</a> losos karas`
         ],
         [
             "https://volpi.ru/losos/karas",
-            `<a href='https://volpi.ru/losos/karas' class=${aClasses}>volpi.ru/losos/karas</a>`
+            `<a href="https://volpi.ru/losos/karas" class=${aClasses}>volpi.ru/losos/karas</a>`
         ],
         [
             "sdlkf sdf ksa https://volpi.ru/losos/karas asdlfk",
-            `sdlkf sdf ksa <a href='https://volpi.ru/losos/karas' class=${aClasses}>volpi.ru/losos/karas</a> asdlfk`
+            `sdlkf sdf ksa <a href="https://volpi.ru/losos/karas" class=${aClasses}>volpi.ru/losos/karas</a> asdlfk`
         ],
         [
             "some big link vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd",
-            `some big link <a href='https://vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd' class=${aClasses}>vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfj...</a>`
+            `some big link <a href="https://vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfjkashdfkhkajsdfhasjkdhfkjashdjkfhaksjhdfkjhasdhfkjahsdkfljhaskjdhfkjhasd" class=${aClasses}>vk.com/fjhasdkjfhajksdhfkadshkfhasjdkhfj...</a>`
         ],
         [
             "andrew-semyonov.vercel.app",
-            `<a href='https://andrew-semyonov.vercel.app' class=${aClasses}>andrew-semyonov.vercel.app</a>`
+            `<a href="https://andrew-semyonov.vercel.app" class=${aClasses}>andrew-semyonov.vercel.app</a>`
+
         ]
     ])("text: '%s' should return '%s'", (text: string, expected: string) => {
         expect(parseLinks(text)).toBe(expected)
