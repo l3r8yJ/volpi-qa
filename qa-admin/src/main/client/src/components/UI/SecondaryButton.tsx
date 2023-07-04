b8f8468beced0fbef19d8f39e61971dacfa7744e
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-04 20:04:44 +0300

    code formatting

--- qa-admin/src/main/client/src/components/UI/SecondaryButton.tsx
+++ qa-admin/src/main/client/src/components/UI/SecondaryButton.tsx
@@ -1,17 +1,19 @@
 import {ButtonHTMLAttributes, FC} from 'react';
 import {getBtnClassesByVariant} from "../../utils/getbtnClassesByVariant";
 
-interface SecondaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface SecondaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: "danger" | "safe" | "default"
 }
 
 export const SecondaryButton: FC<SecondaryButtonProps> = ({children, className, variant = "default", ...props}) => {
-    if(!className) className = ""
+    if (!className) className = ""
     className += getBtnClassesByVariant(variant, "secondary")
     if (!className?.includes("w")) className += " w-full"
     return (
         <button
-            className={`${className ? className : ""} bg-transparent px-4 py-2 rounded-lg text-sm duration-200`} {...props}>
+            className={`${className ? className : ""} bg-transparent px-4 py-2 rounded-lg text-sm duration-200`}
+            {...props}
+        >
             {children}
         </button>
     );
