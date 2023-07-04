1cfeb99fc15448641a7a9f5f6f5b9e3baf93a4ca
l3r8y <l3r8y@duck.com>
2023-07-04 20:20:20 +0300

    Human readable messages

--- qa-rest/src/main/java/ru/volpi/qarest/dto/question/RegisterUnknownQuestion.java
+++ qa-rest/src/main/java/ru/volpi/qarest/dto/question/RegisterUnknownQuestion.java
@@ -6,15 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterUnknownQuestion {
+    @Length(max = 254, message = "Текст вопроса слишком длинный")
     @NotBlank(message = "Текст вопроса не может быть пустым")
     private String text;
 
+    @Length(max = 254, message = "Слишком длинный почтовый адрес")
     @Email(message = "Почта должна иметь правильный формат")
     private String email;
 }
