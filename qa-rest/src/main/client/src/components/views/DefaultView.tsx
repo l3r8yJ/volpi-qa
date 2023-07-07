1c70a771a72d6f10909de099716c31fd7949bc4d
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-07 23:15:28 +0300

    add handling for error on load categories (#178)

--- qa-rest/src/main/client/src/components/views/DefaultView.tsx
+++ qa-rest/src/main/client/src/components/views/DefaultView.tsx
@@ -18,13 +18,19 @@ export const DefaultView: FC = () => {
                 Здравствуйте! Я постараюсь вам помочь и найти ответы на вопросы связанные с поступлением в
                 наш Политех. <br/> Выберите категорию вопроса, который вас интересует.
             </div>
-            <div className={"font-semibold text-lg px-4 pt-4 pb-3 sticky border-b -top-1 bg-white/50 backdrop-blur-sm z-10"}>Категории:</div>
+            <div
+                className={"font-semibold text-lg px-4 pt-4 pb-3 sticky border-b -top-1 bg-white/50 backdrop-blur-sm z-10"}>Категории:
+            </div>
             <div className={"space-y-4 px-4 pt-4"}>
-                {categories.map((category) => (
-                    <CategoryOption key={category.id} category={category}/>
-                ))}
+                {loading === "succeeded"
+                    ? categories.map((category) => (
+                        <CategoryOption key={category.id} category={category}/>
+                    ))
+                    : <div>
+                        Какая жалость, категории кто-то украл <span className={"text-xl"}>😢</span>
+                    </div>
+                }
             </div>
         </>
-    )
-        ;
+    );
 };
