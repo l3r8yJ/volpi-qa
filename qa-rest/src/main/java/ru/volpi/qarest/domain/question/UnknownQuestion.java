d96c9a3596b1aa394254a0a8ebda9d0ab14012b3
l3r8y <l3r8y@duck.com>
2023-07-09 13:01:55 +0300

    imports

--- qa-rest/src/main/java/ru/volpi/qarest/domain/question/UnknownQuestion.java
+++ qa-rest/src/main/java/ru/volpi/qarest/domain/question/UnknownQuestion.java
@@ -1,9 +1,19 @@
 package ru.volpi.qarest.domain.question;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.ZonedDateTime;
 
